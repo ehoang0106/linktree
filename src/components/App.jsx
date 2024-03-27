234ae1f0ed4440dede29d8033ba9df11ac33cad3
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-27 12:52:38 -0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,38 @@
-
+import './App.css'
 
 function App() {
     return (
         <div className="App">
+            <div className="container">
+                <div className="logo">
+                    <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/359337118_774719951319871_3688770500646387034_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_qbWEv_TLPoAX_7cEaG&_nc_ht=scontent-lax3-1.xx&oh=00_AfBbvj8tSV-B_2xs10Do1WpCjDhyfO4wFAPuHWkJcTt9JA&oe=66089D5A" alt="logo avatar" />
+                </div>
+
+                <div className="username">
+                    <h2>@naturewise</h2>
+                </div>
+
+                <div className="items">
+                    <div className="item">
+                        <a href="/">Vitamin D3</a>
+                    </div>
+
+                    <div className="item">
+                        <a href="/">Vitamin C</a>
+                    </div>
+
+                    <div className="item">
+                        <a href="/">Probiotics</a>
+                    </div>
+
+                    <div className="item">
+                        <a href="https://www.google.com/">Omega-3</a>
+                    </div>
+
+                
+                </div>
+            </div>
+
 
         </div>
     );
